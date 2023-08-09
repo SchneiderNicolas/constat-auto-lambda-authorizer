6e710d61ca581b98302ddb6f0b61b2272094d41a
Nicolas Schneider <nicolas.schneider@epitech.eu>
2023-08-09 15:32:30 +0200

    feat: lambda authorizer

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const jwt = require("jsonwebtoken");
+
 exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from updated Lambda!"),
+  const token = event["auth-token"];
+
+  let permission = "Deny";
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    permission = "Allow";
+  } catch (error) {
+    console.error("Invalid token:", error);
+  }
+
+  const authResponse = {
+    principalId: "user",
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: permission,
+          Resource: process.env.AWS_RESSOURCE,
+        },
+      ],
+    },
   };
-  return response;
+  return authResponse;
 };
